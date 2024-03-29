9d8fa02a2d4dfa424eb85b811a1e07a0e17e4fb4
KhaNguyennn <kha.nguyen@nexlab.tech>
2024-03-29 21:27:13 +0700

    first commit

---
+++ hasura/migrations/default/1711722153477_init/up.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users (
+  "id" uuid PRIMARY KEY,
+  "name" TEXT,
+  "email" TEXT,
+  "firebaseId" TEXT,
+  "status" TEXT,
+  "avatar" TEXT,
+  "address" TEXT,
+  "phoneNumber" TEXT,
+  "isMuted" BOOLEAN,
+  "createdAt" TIMESTAMP DEFAULT now(),
+  "updatedAt" TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE dailyTasks (
+  "id" uuid PRIMARY KEY,
+  "userId" uuid,
+  "priority" TEXT,
+  "description" TEXT,
+  "date" TIMESTAMP,
+  "status" TEXT,
+  "taskStatus" TEXT,
+  "month" INTEGER,
+  "year" INTEGER,
+  "createdAt" TIMESTAMP DEFAULT now(),
+  "updatedAt" TIMESTAMP DEFAULT now(),
+  FOREIGN KEY ("userId") REFERENCES users("id")
+);
+
+CREATE TABLE notes (
+  "id" uuid PRIMARY KEY,
+  "userId" uuid REFERENCES users(id),
+  "title" TEXT,
+  "description" TEXT,
+  "createdAt" TIMESTAMP DEFAULT now(),
+  "updatedAt" TIMESTAMP DEFAULT now(),
+  "markedAsDone" BOOLEAN,
+  FOREIGN KEY ("userId") REFERENCES users("id")
+);
